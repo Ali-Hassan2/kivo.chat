cc24db08173a8c5245cde9d7e627a588bfd9edb0
Ali Hassan <alihassan26032004@gmail.com>
2025-10-18 18:36:52 +0500

    resend-build-fix

--- src/resend/resend-call.ts
+++ src/resend/resend-call.ts
@@ -1,11 +1,15 @@
 import { Resend } from 'resend'
 
-const resend_uri = process.env.RESEND_API_KEY
-
-if (!resend_uri) {
-  throw new Error('Resend enviornment variables not found.')
+let resend: Resend | null = null
+const getResendClient = (): Resend => {
+  if (!resend) {
+    const apiKey = process.env.RESEND_API_KEY
+    if (!apiKey) {
+      throw new Error('Resend environment variable RESEND_API_KEY not found.')
+    }
+    resend = new Resend(apiKey)
+  }
+  return resend
 }
 
-const resend = new Resend(resend_uri)
-
-export { resend }
+export { getResendClient }
