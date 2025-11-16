bc8616890abd7a321286dafbe512f9cb344840be
Ali Hassan <alihassan26032004@gmail.com>
2025-11-16 17:20:44 +0500

    mode-to-change-identity

---
+++ src/helpers/user-anon-form.ts
@@ -0,0 +1,16 @@
+const hiddenUserMock = {
+  username: 'user',
+  fullName: 'kivoUser',
+  bio: 'kivoChattingUser',
+  pfp: {
+    public_id: 'nothing.img',
+    url: 'nothing_found.img',
+  },
+  coverImage: {
+    public_id: 'nothing_cover',
+    url: 'nothing_found_in_cover',
+  },
+}
+
+export { hiddenUserMock }
+
