044f3f0fba541363ea03a56ae0d37bf1e56cc246
Ali Hassan <alihassan26032004@gmail.com>
2025-11-18 17:26:47 +0500

    TODO:

--- src/app/api/block/unblock/route.ts
+++ src/app/api/block/unblock/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { z } from 'zod'
+import { UserModel } from '@/entities'
 import { connect_db } from '@/settings'
 import { authOptions } from '../../auth/[...nextauth]/options'
 
@@ -20,7 +21,6 @@ async function POST(request: Request) {
         },
       )
     }
-
     const session = await getServerSession(authOptions)
     if (!session?.user?.username) {
       return NextResponse.json(
@@ -45,6 +45,22 @@ async function POST(request: Request) {
         errors: parseResult.error?.issues.map((err) => err?.message),
       })
     }
-    
+
+    const uid = session?.user?._id
+    const [user, unblocked] = await Promise.all([
+      UserModel.findById(uid),
+      UserModel.findOne({ username: username }),
+    ])
+    if (!user || unblocked) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Instances not found',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
   } catch (error) {}
 }
