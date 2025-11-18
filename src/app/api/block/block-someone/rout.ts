0cf92e8ceaab3524ff5f1417780e83f5fe40849c
Ali Hassan <alihassan26032004@gmail.com>
2025-11-18 16:45:57 +0500

    making parallel calls

--- src/app/api/block/block-someone/rout.ts
+++ src/app/api/block/block-someone/rout.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { z } from 'zod'
-import { authOptions } from '../../auth/[...nextauth]/options'
 import { UserModel } from '@/entities'
+import { authOptions } from '../../auth/[...nextauth]/options'
 
 const usernameQuerySchema = z
   .string()
@@ -45,7 +45,21 @@ async function POST(request: Request) {
         errors: parseResult.error.issues.map((err) => err?.message),
       })
     }
-    const user = await UserModel.findById(uid)
-    
+
+    const [user, blocked] = await Promise.all([
+      UserModel.findById(uid),
+      UserModel.findOne({ username: parseResult.data }),
+    ])
+    if (!user || !blocked) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Instances for Users not found.',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
   } catch (error) {}
 }
