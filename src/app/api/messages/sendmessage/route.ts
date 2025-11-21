ae19e96ace569c50fe0f04a89270d3a6efe99298
Ali Hassan <alihassan26032004@gmail.com>
2025-11-21 21:52:23 +0500

    working-with-messages

--- src/app/api/messages/sendmessage/route.ts
+++ src/app/api/messages/sendmessage/route.ts
@@ -1,13 +1,87 @@
-import { getCurrentUser } from "@/helpers";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
+import { Types } from 'mongoose'
+import { ConversationModel, UserModel } from '@/entities'
+import { ObjectIdGuard } from '@/guards'
+import { getCurrentUser } from '@/helpers'
 
-async function POST(request:Request){
- 
+async function POST(request: Request) {
+  if (request.method !== 'POST') {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Method not allowed',
+      },
+      {
+        status: 405,
+      },
+    )
+  }
+  try {
     const user = getCurrentUser()
-    if(!user){
+    if (!user) {
+      return NextResponse.json({
+        success: false,
+        message: 'Session not found Please login first',
+      })
+    }
+    const body = request.body
+    const { searchParams } = new URL(request.url)
+    const rawId = searchParams.get('receiverId') || ''
+    const decodedReceiverId = decodeURIComponent(rawId)
+    const parsedId = ObjectIdGuard.safeParse(decodedReceiverId)
+    if (!parsedId.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Validation failed',
+          error: parsedId.error.issues.map((err) => err?.message),
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const { content } = request.body
+    if (!content) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Message Content is required',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+    // get the status is receiver is accepting messages or not
+    const receiverUser = await UserModel.findById(receiverId)
+    if(!receiverUser){
+        return NextResponse.json(
+        {
+            success:false,
+            message:"No receiver found"
+        },{
+            status:400
+        }
+        )
+    }
+    const isStatusIsValid = receiverUser.isAcceptingMessages
+    if(!isStatusIsValid){
         return NextResponse.json({
             success:false,
-            message:"Session not found Please login first"
+            message:"User is not accepting messages"
+        },{
+            status:400
         })
     }
+    let isPreviousConversataion = await ConversationModel.findOne({
+      participants: {
+        $all: [user?._id as Types.ObjectId, receiverId as Types.ObjectId],
+      },
+    })
+    if(!isPreviousConversataion){
+        await ConversationModel.
+    }
+  } catch (error) {}
 }
