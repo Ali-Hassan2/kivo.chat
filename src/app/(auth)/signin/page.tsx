1d0642c666b5d8746bc4ccbb913a0ea84f22cd6a
Ali Hassan <alihassan26032004@gmail.com>
2025-12-03 22:26:18 +0500

    auth/v3

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 const page = () => {
   return (
-    <div className="h-[100vh] w-[100vw] border-4 border-red-500 text-white">
-      Hello this is Ali Hassan
+    <div className="flex h-[100vh] w-[100vw] items-center justify-center border-4 border-red-500 text-white">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle>Kivo Auth</CardTitle>
+          <CardDescription>
+            Login to your account with valid credentials.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+            
+        </CardContent>
+      </Card>
     </div>
   )
 }
