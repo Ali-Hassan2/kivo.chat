89a7b37ecec9ffe852789e09c1a617b4e8542d1f
Ali Hassan <alihassan26032004@gmail.com>
2025-12-03 22:31:40 +0500

    auth/v4- guard-updated

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as z from zod 
 import {
   Card,
   CardContent,
@@ -6,8 +7,17 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { useForm } from 'react-hook-form'
+import { signInGuard } from '@/guards'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const page = () => {
+    const form = useForm<z.infer<typeof signInGuard>>({
+        resolver: zodResolver(signInGuard),
+        defaultValues:{
+            ""
+        }
+    })
   return (
     <div className="flex h-[100vh] w-[100vw] items-center justify-center border-4 border-red-500 text-white">
       <Card className="w-[400px]">
@@ -18,7 +28,7 @@ const page = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-            
+
         </CardContent>
       </Card>
     </div>
