548d82576347ad5a55e8505b11f0dd4377ecb054
Ali Hassan <alihassan26032004@gmail.com>
2025-12-03 22:19:36 +0500

    signin from next-auth

---
+++ src/elements/login-form/login-form.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return <div className="h-[100vh] w-[100vw] border-4 border-red-500">
+    Hello from Ali
+  </div>
+}
+
+export default LoginForm
