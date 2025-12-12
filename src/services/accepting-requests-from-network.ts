63a09b6328c799f8d9ae34a82f76b49054d61e12
Ali Hassan <alihassan26032004@gmail.com>
2025-12-12 12:17:58 +0500

    accepting-connection/v2

---
+++ src/services/accepting-requests-from-network.ts
@@ -0,0 +1,17 @@
+import { ApiResponse } from '@/types'
+
+interface AcceptingRequestFromOverallNetworkProps {
+  signal: AbortSignal
+  requestId: string
+}
+
+const AcceptRequestFromOverallNetwork = async ({
+  signal,
+  requestId,
+}:AcceptingRequestFromOverallNetworkProps): Promise<ApiResponse> => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
