b6e80f6e180a7f0926780470a6595c37894dd767
Ali Hassan <alihassan26032004@gmail.com>
2025-12-19 18:55:34 +0500

    have to integarate on click/v17

--- src/elements/all-friends/all-friends.tsx
+++ src/elements/all-friends/all-friends.tsx
@@ -47,7 +47,7 @@ const AllFriends = ({
       showToast(errorMessage, 'error')
     }
     if(errorForGettingAllFriendsOverallNetwork){
-      
+      showToast
     }
   },[UnfriendSomeoneOverallNetworkResponse])
   return (
