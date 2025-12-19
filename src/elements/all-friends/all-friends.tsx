8a424b8a49a248ac84036bf21d717734bcfd80bd
Ali Hassan <alihassan26032004@gmail.com>
2025-12-19 19:32:11 +0500

    hook/service/element/view-completed/v2-todo-added

--- src/elements/all-friends/all-friends.tsx
+++ src/elements/all-friends/all-friends.tsx
@@ -101,7 +101,7 @@ const AllFriends = ({
                   </Button>
                 </Box>
               </Box>
-              // 
+              // TODO: 
             )
           })}
         </Box>
