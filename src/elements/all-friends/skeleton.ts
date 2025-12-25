ce49e9cccf29757e9d1175c93410b63be67ad994
Ali Hassan <alihassan26032004@gmail.com>
2025-12-25 14:18:52 +0500

    version 02/skeleton for blocked users

---
+++ src/elements/all-friends/skeleton.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const skeleton = () => {
+  return (
+    <div>skeleton</div>
+  )
+}
+
+export default skeleton
--- src/elements/block-board/block-board.tsx
+++ src/elements/block-board/block-board.tsx
@@ -5,8 +5,21 @@ import { Box, Flex } from '@radix-ui/themes'
 import { BoardUpperHeader, FriendsIcon } from '@/components'
 import { NetworkIcon } from '@/components/icons/network'
 import { ALL_BLOCKED_FRIENDS_OVERALLNETWORK_BOARD } from '@/constants/objects-to-iterate'
+import { IBlocks } from '@/types'
 
-const BlockBoard = () => {
+interface BlockBoardProps {
+  getBlocksUsersOverallNetwork: IBlocks[]
+  errorMessageForGettingBlockedUsers: string | null
+  isGettingBlockedUsers: boolean
+  gettingAllBlockedUsers: () => Promise<any> | void
+}
+
+const BlockBoard = ({
+  getBlocksUsersOverallNetwork,
+  errorMessageForGettingBlockedUsers,
+  isGettingBlockedUsers,
+  gettingAllBlockedUsers,
+}: BlockBoardProps) => {
   return (
     <Box className="w-full">
       <Flex direction="column">
@@ -19,6 +32,7 @@ const BlockBoard = () => {
           ButtonTwo={ALL_BLOCKED_FRIENDS_OVERALLNETWORK_BOARD.ButtonTwo}
           lineWidth={ALL_BLOCKED_FRIENDS_OVERALLNETWORK_BOARD.lineWidth}
         />
+        
       </Flex>
     </Box>
   )
