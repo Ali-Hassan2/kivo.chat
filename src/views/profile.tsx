5740528eb8ea3c5727541880500937956df75861
Ali Hassan <alihassan26032004@gmail.com>
2025-12-27 22:06:13 +0500

    sidebar for profile pages

---
+++ src/views/profile.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { Box } from '@radix-ui/themes'
+
+const ProfileView = () => {
+  return (
+    <Box className="borde-red-500 h-full w-[100vw] border">
+      Hello to your profile
+    </Box>
+  )
+}
+
+export { ProfileView }
