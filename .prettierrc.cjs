8de124f4dc63a499615bf383203f368967a72fc9
Ali Hassan <alihassan26032004@gmail.com>
2026-03-10 20:39:28 +0500

    updated formatting build

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,27 +1,27 @@
 /** @type {import("prettier").Config} */
 const config = {
-  arrowParens: "always",
+  arrowParens: 'always',
   printWidth: 80,
   singleQuote: true,
   semi: false,
-  trailingComma: "all",
+  trailingComma: 'all',
   tabWidth: 2,
 
   plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
   ],
 
   importOrder: [
-    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-    "^(next/(.*)$)|^(next$)",
-    "<THIRD_PARTY_MODULES>",
-    "^@/(.*)$",
-    "^[./]",
+    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '^@/(.*)$',
+    '^[./]',
   ],
 
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  tailwindConfig: "./tooling/tailwind-config",
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  tailwindConfig: './tooling/tailwind-config',
 }
 
 module.exports = config
